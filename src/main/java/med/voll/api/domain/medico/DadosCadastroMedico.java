aacd56fc532f319b89dd30a7e93b3a66364ee268
Gutemberg Siqueira <gutembergsiqueira96@gmail.com>
2025-08-07 13:20:50 -0300

    Add personalização nas mensagens de erro

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -8,24 +8,16 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        @NotBlank
         String nome,
 
-        @NotBlank
-        @Email
         String email,
 
-        @NotBlank
         String telefone,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{4,6}")
         String crm,
 
-        @NotNull
         Especialidade especialidade,
 
-        @NotNull
         @Valid
         DadosEndereco endereco
         ) {
