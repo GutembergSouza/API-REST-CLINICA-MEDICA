1ce5049e89208b7c9b57be26f6e3f3a82ac13af0
Gutemberg Siqueira <gutembergsiqueira96@gmail.com>
2025-08-07 13:20:50 -0300

    Add personalização nas mensagens de erro

---
+++ src/main/java/med/voll/api/domain/paciente/DadosPaciente.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosPaciente(
+        @NotBlank(message = "{nome.obrigatorio}")
+        String nome,
+
+        @NotBlank(message = "{email.obrigatorio}")
+        @Email(message = "{email.invalido}")
+        String email,
+
+        @NotBlank(message = "{telefone.obrigatorio}")
+        String telefone,
+
+        @NotBlank(message = "{cpf.obrigatorio}")
+        String cpf,
+
+        @Valid
+        @NotNull(message = "{endereco.obrigatorio}")
+        DadosEndereco endereco
+        ){}
