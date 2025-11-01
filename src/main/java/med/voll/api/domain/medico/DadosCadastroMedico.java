da1a7aebd035ebe2006fb39b689566bab8377ad6
gu <gutembergsiqueira96@gmail.com>
2025-10-31 22:40:36 -0300

    Adicionar mensagens de erro personalidas ao cadastro de médicos

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -8,12 +8,14 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
+        @NotBlank(message = "{nome.obrigatorio}")
         String nome,
 
+        @NotBlank(message = "{email.obrigatorio}")
         String email,
-
+        @NotBlank(message = "{telefone.obrigatorio}")
         String telefone,
-
+        @NotBlank(message = "{crm.obrigatorio}")
         String crm,
 
         Especialidade especialidade,
